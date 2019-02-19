252738bca1b58b632b3748ae4028f5f55da64e94
Mohammad <mghmgh1281375@gmail.com>
2019-02-19 21:50:57 +0330

    Update

--- smart_threshold.py
+++ smart_threshold.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 
 c = 1
+d = 1
 gamma_list = [0.1, 0.2, 0.5, 1., 5., 10.]
 image = cv2.imread('./resources/photo_2019-02-19_21-19-39.jpg', 0)
 
@@ -16,6 +17,13 @@ for gamma in gamma_list:
 
     # thresholding
     thresh = c * np.power(image, gamma)
+    cv2.imshow('gamma-{:.3}'.format(gamma), thresh)
+    cv2.waitKey()
+
+for gamma in gamma_list:
+
+    # thresholding
+    thresh = c / (np.power(image, gamma) + d)
     cv2.imshow('gamma-{:.3}'.format(gamma), thresh)
     cv2.waitKey()
 
