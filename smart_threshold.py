b3fcc813ea4e6b1f766efdae5e8d4fe048455845
Mohammad <mghmgh1281375@gmail.com>
2019-02-19 22:05:27 +0330

    Update

--- smart_threshold.py
+++ smart_threshold.py
@@ -18,6 +18,7 @@ for gamma in gamma_list:
     # thresholding
     thresh = c * np.power(normalized, gamma)
     cv2.imshow('gamma-{:.3}'.format(gamma), thresh)
+    cv2.imwrite('./resources/gamma-{:.3}.png'.format(gamma), thresh * 255)
     cv2.waitKey()
 
 for gamma in gamma_list:
